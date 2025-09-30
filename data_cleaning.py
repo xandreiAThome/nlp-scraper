28c6bc616eed6455b448063225f798758ea8158a
Parum-Lucis <161942643+Parum-Lucis@users.noreply.github.com>
2025-10-01 01:12:04 +0800

    feat: add functional verb segmentation and segment segmentation.

--- data_cleaning.py
+++ data_cleaning.py
@@ -5,20 +5,22 @@ from pathlib import Path
 from concurrent.futures import ProcessPoolExecutor
 
 VERSE_RULES = [
-    (r"(</span>|</div>)", r"\1\n"),  # TOKENIZE closing span/div
-    (r".*(<span class=\"ChapterContent_label__R2PLt\">((\d|,|-)+)</span>)", r"@\2"),  # EXTRACT VERSE NUMBER
-    (r".*(<span class=\"ChapterContent_content__RrUqA\">([^<]+)</span>)", r"\2"),  # EXTRACT VERSE CONTENT
-    # (r"^.*[<>].*$\R?", r""),  # Get rid of unnecessary HTML
-    (r"\r\n?", r"\n"),  # UNIFY NEWLINES
-    (r" +", r" "),  # NORMALIZE WHITESPACE
-    (r"\[|\]", r""),  # REMOVE SQUARE BRACKETS
-    (r"Profetizada por Jesús Anunciada por el ángel Sentido cristiano Véanse la tabla y en la", r""),
-    # REMOVE SPANISH REF
-    (r"@((\d|,|-)+)([^@]+)", r"\n\\Verse \1:\3"),  # SEGMENT BY VERSE
-    (r"^\s*$\R?", r""),  # REMOVE EMPTY LINES
+    (r"<span class=\"ChapterContent_label__R2PLt\">([\d,-]+)</span>", "\r\n@$1\r\n"),  # Extract Verse Number
+    (r"<span class=\"ChapterContent_content__RrUqA\">([^<]+)<\/span>", "\r\n%$1\r\n"),  # Extract Verse Content
+    (r"^[^@%].*$\R?", r""),  # Clean-up of unnecessary HTML
+    (r"%|\R|\[|\]|Profetizada por Jesús Anunciada por el ángel Sentido cristiano Véanse la tabla y en la", r" "),  # Get rid of unnecessary elements
+    (r" +", r" "),  # Clean-up of unnecessary WHITESPACE
+    (r"@([\d,-]+)([^@]+)", "\nVerse $1:$2"),  # Segment by Verse
 ]
 
-compiled_rules = [(pcre2.compile(p, pcre2.MULTILINE), r) for p, r in VERSE_RULES]
+SENTENCE_RULES = [
+    (r"<span class=\"ChapterContent_content__RrUqA\">([^<]+)<\/span>", "\r\n%$1\r\n"), # Extract Verse Content
+    (r"^[^%].*$\R?", r""), # Clean-up of unnecessary HTML
+    (r"%|\R|\[|\]|Profetizada por Jesús Anunciada por el ángel Sentido cristiano Véanse la tabla y en la ", r" "), # Get rid of unnecessary elements
+    (r" +", r" "),  # Clean-up of unnecessary WHITESPACE
+    (r" ((?-i)[^\.\!\?]+[\.\!\?]”?(?=\s+[A-Z]))", "$1\n"), # Segment by Sentence
+    (r"^\s|(?<=“) ", "") # Clean-up of generated WHITESPACE
+]
 
 def apply_rules(text: str, rules) -> str:
     for pattern_str, repl in rules:
@@ -32,6 +34,7 @@ def process_file(file, input_path, verses_path, sentences_path):
 
     print("Processing: " + file.name)
     verse_segment = apply_rules(text, VERSE_RULES)
+    sentence_segment = apply_rules(text, SENTENCE_RULES)
 
     relative = file.relative_to(input_path).with_suffix(".txt")
     verse_file = verses_path / relative
@@ -43,17 +46,20 @@ def process_file(file, input_path, verses_path, sentences_path):
     with verse_file.open("w", encoding="utf-8") as f:
         f.write(verse_segment)
 
+    with sentence_file.open("w", encoding="utf-8") as f:
+        f.write(sentence_segment)
+
     return str(relative)
 
 def choose_workers(files):
     total_size = sum(f.stat().st_size for f in files)
     avg_size = total_size / len(files)
 
-    if len(files) > 500 and avg_size < 50_000:  # many small files (<50 KB)
+    if len(files) > 500 and avg_size < 50_000:
         print("Using single process")
         return 1
     else:
-        max_workers = min(os.cpu_count() or 4, 4)  # cap at 4
+        max_workers = min(os.cpu_count() or 4, 4)
         print(f"Using {max_workers} workers")
         return max_workers
 
@@ -73,8 +79,6 @@ def segment_verses(input_folder, verses_folder, sentence_folder, workers=None):
         workers = choose_workers(files)
 
     start = time.time()
-
-    files = files[:1]
 
     if workers == 1:
         for f in files:
